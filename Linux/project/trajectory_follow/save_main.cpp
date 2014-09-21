e852cda05b8c971f050c51e3e4ac2175e08904a4
klowrey <klowrey@5fbd6238-4cb4-b94d-a141-084efb5cd025>
2014-09-21 22:30:43 +0000

    added in some code for imu

--- Linux/project/trajectory_follow/save_main.cpp
+++ Linux/project/trajectory_follow/save_main.cpp
@@ -67,6 +67,74 @@ double q1=-0.3;
 double q2=0.3;
 double q3=0.65;
 
+void MadgwickAHRSupdateIMU(double gx, double gy, double gz, double ax, double ay, double az, double dt) {
+	double recipNorm;
+	double s0, s1, s2, s3;
+	double qDot1, qDot2, qDot3, qDot4;
+	double _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+
+	// Rate of change of quaternion from gyroscope
+	qDot1 = 0.5 * (-q1 * gx - q2 * gy - q3 * gz);
+	qDot2 = 0.5 * (q0 * gx + q2 * gz - q3 * gy);
+	qDot3 = 0.5 * (q0 * gy - q1 * gz + q3 * gx);
+	qDot4 = 0.5 * (q0 * gz + q1 * gy - q2 * gx);
+
+	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+	//if(!((ax == 0.0) && (ay == 0.0) && (az == 0.0))) {
+
+	// Normalise accelerometer measurement
+	recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+	ax *= recipNorm;
+	ay *= recipNorm;
+	az *= recipNorm;   
+
+	// Auxiliary variables to avoid repeated arithmetic
+	_2q0 = 2.0 * q0;
+	_2q1 = 2.0 * q1;
+	_2q2 = 2.0 * q2;
+	_2q3 = 2.0 * q3;
+	_4q0 = 4.0 * q0;
+	_4q1 = 4.0 * q1;
+	_4q2 = 4.0 * q2;
+	_8q1 = 8.0 * q1;
+	_8q2 = 8.0 * q2;
+	q0q0 = q0 * q0;
+	q1q1 = q1 * q1;
+	q2q2 = q2 * q2;
+	q3q3 = q3 * q3;
+
+	// Gradient decent algorithm corrective step
+	s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+	s1 = _4q1 * q3q3 - _2q3 * ax + 4.0 * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+	s2 = 4.0 * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+	s3 = 4.0 * q1q1 * q3 - _2q1 * ax + 4.0 * q2q2 * q3 - _2q2 * ay;
+	recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+	s0 *= recipNorm;
+	s1 *= recipNorm;
+	s2 *= recipNorm;
+	s3 *= recipNorm;
+
+	// Apply feedback step
+	qDot1 -= beta * s0;
+	qDot2 -= beta * s1;
+	qDot3 -= beta * s2;
+	qDot4 -= beta * s3;
+	//}
+
+	// Integrate rate of change of quaternion to yield quaternion
+	q0 += qDot1 * dt;
+	q1 += qDot2 * dt;
+	q2 += qDot3 * dt;
+	q3 += qDot4 * dt;
+
+	// Normalise quaternion
+	recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	q0 *= recipNorm;
+	q1 *= recipNorm;
+	q2 *= recipNorm;
+	q3 *= recipNorm;
+}
+
 void MahonyAHRSupdateIMU(double gx, double gy, double gz, double ax, double ay, double az, double dt) {
 	double recipNorm;
 	double integralFBx=0.0, integralFBy=0.0, integralFBz=0.0;
@@ -130,19 +198,11 @@ void MahonyAHRSupdateIMU(double gx, double gy, double gz, double ax, double ay, 
 
 	// Normalise quaternion
 	//recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-	//recipNorm = 1.0/sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-	//q0 *= recipNorm;
-	//q1 *= recipNorm;
-	//q2 *= recipNorm;
-	//q3 *= recipNorm;
-
-	//quat o;
-	//o.q0 = q0;
-	//o.q1 = q1;
-	//o.q2 = q2;
-	//o.q3 = q3;
-
-	//return o;
+	recipNorm = 1.0/sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	q0 *= recipNorm;
+	q1 *= recipNorm;
+	q2 *= recipNorm;
+	q3 *= recipNorm;
 }
 
 void initial_pose(double* joints, CM730 * cm730) {
@@ -425,16 +485,16 @@ int main(int argc, char* argv[])
 	}
 
 	/*
-		double* vel_line = NULL;
-		if (vel_file->empty() == false) {
-		vel_line = open_read_binary(vel_file->c_str(), LINE_SIZE);
+	   double* vel_line = NULL;
+	   if (vel_file->empty() == false) {
+	   vel_line = open_read_binary(vel_file->c_str(), LINE_SIZE);
 
-		if (vel_line == NULL) {
-		printf("Bad velocity file\n");
-		return 0;
-		}
-		}
-		*/
+	   if (vel_line == NULL) {
+	   printf("Bad velocity file\n");
+	   return 0;
+	   }
+	   }
+	   */
 
 	double* joint_data;
 	double* prev_joint;
@@ -516,18 +576,18 @@ int main(int argc, char* argv[])
 
 			// can print out 20 joint datas
 			/*
-				printf("t: %1.3f; ", prev_joint[0]);
-				for (int joint=1; joint<20; joint++) {
-				printf("%1.3f ", prev_joint[joint]);
-				}
-				printf("\n");
+			   printf("t: %1.3f; ", prev_joint[0]);
+			   for (int joint=1; joint<20; joint++) {
+			   printf("%1.3f ", prev_joint[joint]);
+			   }
+			   printf("\n");
 
-				printf("t: %1.3f; ", joint_data[0]);
-				for (int joint=1; joint<20; joint++) {
-				printf("%1.3f ", joint_data[joint]);
-				}
-				printf("\n");
-				*/
+			   printf("t: %1.3f; ", joint_data[0]);
+			   for (int joint=1; joint<20; joint++) {
+			   printf("%1.3f ", joint_data[joint]);
+			   }
+			   printf("\n");
+			   */
 
 			printf("%f ??? %f\n", time_passed, timestamp);
 			while ( time_passed < timestamp) {
@@ -626,7 +686,8 @@ int main(int argc, char* argv[])
 				double accel_z = accel2ms2(cm730.m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_ACCEL_Z_L));
 
 				printf("dt_interp: %f\n", dt_interp);
-				MahonyAHRSupdateIMU(gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, dt_interp);
+				//MahonyAHRSupdateIMU(gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, dt_interp);
+				MadgwickAHRSupdateIMU(gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, dt_interp);
 				out << time<<","<< q0<<","<< q1<<","<< q2<<","<< q3<<std::endl;
 
 				set_positions(percent, interp);
